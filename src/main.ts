d558bbca491012ee48e0959b32f11cab1fb7dac4
jwvonk <jwavonk@gmail.com>
2023-10-09 17:25:16 -0700

    step 1 complpete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Will's game";
+const buttonEmoji = "🌴";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const mainButton = document.createElement("button");
+mainButton.innerHTML = buttonEmoji;
+app.append(mainButton);
